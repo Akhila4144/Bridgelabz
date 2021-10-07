6476ce7cfa333ed8f93eb00befc8e4a97b6e53df
Akhila4144 <orugantiakhila34@gmail.com>
2021-10-07 12:26:54 +0530

    This file has modified for adding parttime

--- calculateEmpWage.sh
+++ calculateEmpWage.sh
@@ -1,4 +1,4 @@
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));
 WAGE_PER_HOUR=20;
 
 if [ $isPresent -eq 1 ]
@@ -7,9 +7,17 @@ then
 	WorkingHour=8;
 	Per_Day_Salary=$(( $WorkingHour * $WAGE_PER_HOUR ));
 	echo " Daily Emp Wage is $Per_Day_Salary ";
-else
+elif [ $isPresent -eq 0 ]
+then
 	echo " Employee is Absent ";
 	WorkingHour=0;
 	Per_Day_Salary=$(( $WorkingHour * $WAGE_PER_HOUR ));
 	echo " Daily Emp Wage is $Per_Day_Salary ";
+else
+	echo " Employee is doing work as parttime "
+	WorkingHour=4;
+	Per_Day_Salary=$(( $WorkingHour * $WAGE_PER_HOUR ));
+	echo "Daily Emp Wage is $Per_Day_Salary ";
+
 fi
+
